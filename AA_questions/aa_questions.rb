fd8222adc820f375471af438be93855663b4bc79
guanw88 <guanw88@gmail.com>
2018-11-06 12:16:31 -0800

    Set up database connections and class methods for Question, User, and Rely classes

---
+++ AA_questions/aa_questions.rb
@@ -0,0 +1,175 @@
+require 'sqlite3'
+require 'singleton'
+
+
+# SQLite3::Database.new( "questions.db" ) do |db|
+#   db.execute( "select * from table" ) do |row|
+#     p row
+#   end
+# end
+
+class QuestionsDatabase < SQLite3::Database
+  include Singleton
+
+  def initialize
+    super('questions.db')
+    self.type_translation = true
+    self.results_as_hash = true
+  end
+end
+
+
+class User 
+  attr_accessor :fname, :lname
+  
+  def self.find_by_id(id)
+    user = QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM
+        users
+      WHERE
+        id = ?
+    SQL
+    return nil unless user.length > 0
+
+    User.new(user.first)
+  end 
+  
+  def self.find_by_name(fname, lname)
+    users = QuestionsDatabase.instance.execute(<<-SQL, fname, lname)
+      SELECT
+        *
+      FROM
+        users
+      WHERE
+        fname = ? AND lname = ?
+    SQL
+    return nil unless users.length > 0
+
+    users.map { |user| User.new(user) }
+  end 
+  
+  def initialize(options)
+    @id = options['id']
+    @fname = options['fname']
+    @lname = options['lname']
+  end 
+  
+  
+end
+
+class Question 
+  attr_accessor :title, :body, :author_id
+  
+  def self.find_by_id(id)
+    question = QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM
+        questions
+      WHERE
+        id = ?
+    SQL
+    return nil unless question.length > 0
+
+    Question.new(question.first)
+  end 
+  
+  def self.find_by_author_id(author_id)
+    questions = QuestionsDatabase.instance.execute(<<-SQL, author_id)
+      SELECT
+        *
+      FROM
+        questions
+      WHERE
+        author_id = ?
+    SQL
+    return nil unless questions.length > 0
+
+    questions.map { |question| Question.new(question) }
+  end 
+  
+  
+  def initialize(options) 
+    @id = options['id']
+    @title = options['title']
+    @body = options['body']
+    @author_id = options['author_id']
+  end 
+  
+  def author 
+  end 
+  
+  def replies 
+  end 
+  
+end 
+
+class Reply 
+  attr_accessor :body, :question_id, :parent_reply_id, :user_id
+  
+  def self.find_by_id(id)
+    reply = QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM
+        replies
+      WHERE
+        id = ?
+    SQL
+    return nil unless reply.length > 0
+
+    Reply.new(reply.first)
+  end 
+  
+  def self.find_by_user_id(user_id)
+    replies = QuestionsDatabase.instance.execute(<<-SQL, user_id)
+      SELECT
+        *
+      FROM
+        replies
+      WHERE
+        user_id = ?
+    SQL
+    return nil unless replies.length > 0
+
+    replies.map { |reply| Reply.new(reply) }
+  end 
+  
+  def self.find_by_question_id(question_id)
+    replies = QuestionsDatabase.instance.execute(<<-SQL, question_id)
+      SELECT
+        *
+      FROM
+        replies
+      WHERE
+        question_id = ?
+    SQL
+    return nil unless replies.length > 0
+
+    replies.map { |reply| Reply.new(reply) }
+  end 
+  
+  def initialize(options) 
+    @id = options['id']
+    @body = options['body']
+    @question_id = options['question_id']
+    @parent_reply_id = options['parent_reply_id']
+    @user_id = options['user_id']
+  end 
+end 
+
+class Follow 
+  def initialize(options)
+    @user_id = options['user_id']
+    @question_id = options['question_id']
+  end 
+end 
+
+class Like 
+  def initialize(options) 
+    @user_id = options['user_id']
+    @question_id = options['question_id']
+  end 
+end 
